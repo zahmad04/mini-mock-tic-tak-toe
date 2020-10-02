b670aa4d60f91ffacc3bdde2766ae2f4cef62d54
Leroy Salih <leroy.salih@gmail.com>
2020-10-02 03:51:56 +0000

    Working

--- submit.py
+++ submit.py
@@ -3,4 +3,4 @@ from subprocess import run
 print("Submitting Files")
 run(["git", "add", "."])
 run(["git", "commit", "-m", "Working"])
-run(["git", "push", "orgin"])
+run(["git", "push", "origin"])
