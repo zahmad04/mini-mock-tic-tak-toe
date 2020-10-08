a60b084066f71d2dfc32ab268d2f0decd9d59cfa
Leroy Salih <leroy.salih@gmail.com>
2020-10-08 07:09:47 +0000

    Uploading work.

--- starter.py
+++ starter.py
@@ -1,10 +1,7 @@
-def mySum (a, b):
-    return a + b
-
-pupil_age = 15
-
 if __name__ == "__main__":
     
+    # Should output the first and last letter of the word entered by 
+    # the user.
     word = input("Enter a word.")
     print("The first letter is {0}".format(word[0]))
-    print("The last letter is {0}".format(word[-1:]))
+    print("The last letter is {0}".format(word[-2:]))
